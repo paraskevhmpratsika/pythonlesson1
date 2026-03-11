49ce3ca428f0d3945569068bfa9d04c25354476d
paraskevhmpratsika <paraskevhmpratsika@gmail.com>
2026-03-11 11:03:45 +0200

    Create lesson3_2.py
    Απάντηση Μάριου

---
+++ lesson3_2.py
@@ -0,0 +1,9 @@
+message = input("Πληκτρολογήστε ένα μήνυμα: ")
+
+message_length = len(message)
+print(f"Το μήκος του μηνύματος είναι: {message_length}")
+
+print(f"Το μήνυμα με κεφαλαία: {message.upper()}")
+
+has_python = "python" in message
+print(f"Υπάρχει η λέξη 'python' στο μήνυμα; {has_python}")
